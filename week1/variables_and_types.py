83a71735da138a83deafd2473f1bf4e75c55cd10
candle-wisp <willow.faye@icloud.com>
2026-03-16 09:13:28 -0700

    Week 1: variables and types script

---
+++ week1/variables_and_types.py
@@ -0,0 +1,14 @@
+# Test user information
+username = "standard_user"
+password = "secret_passphrase"
+exp_title = "My Labs"
+active = True
+items = 6
+
+# Print using f-strings
+print(f" Hello. Your username is {username}." ) 
+print(f"Password is {password}." )
+print(f"Title is {exp_title}.")
+print(f"Are you active? {active} ")
+print(f"How many? {items}")
+
